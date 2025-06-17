2ca0cefd55a2fc05b73dc96aa4d2ae02946cc9b5
AxelMontlahuc <axel.montlahuc@proton.me>
2025-06-17 19:36:55 +0200

    Added generation page

---
+++ website/src/pages/generate.tsx
@@ -0,0 +1,26 @@
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+export default function Generate() {
+  return (
+    <>
+      <main>
+          <div className="flex flex-col items-center m-[20px] mt-[40px]">
+            <h1 className="text-6xl m-[10px]">Generate Brainrot</h1>
+          </div>
+          <div className="flex flex-col items-center w-100vw m-[40px]">
+            <div className="w-[1000px] flex flex-row justify-between">
+              <div className="w-[360px] h-[640px] bg-gray-900"></div>
+              <div className="h-[640px] w-[600px] flex flex-col justify-between">
+                <Textarea className="w-[600px] h-[560px]" placeholder="Type in your script or your script idea here. " />
+                <div className="flex flex-row justify-between h-[50px]">
+                  <Button className="w-[285px] h-[50px]" variant="secondary">Generate Scipt</Button>
+                  <Button className="w-[285px] h-[50px]">Generate Video</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+      </main>
+    </>
+  );
+}
