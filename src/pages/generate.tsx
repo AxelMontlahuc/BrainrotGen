294f856be037010d760c87f30b8b7b0907bc53c7
AxelMontlahuc <axel.montlahuc@proton.me>
2025-06-17 21:24:50 +0200

    Refactored the code

--- src/pages/generate.tsx
+++ src/pages/generate.tsx
@@ -10,7 +10,7 @@ export default function Generate() {
           </div>
           <div className="flex flex-col items-center w-100vw m-[40px]">
             <div className="w-[1000px] flex flex-row justify-between">
-              <div className="w-[360px] h-[640px] bg-gray-900"></div>
+              <div className="w-[360px] h-[640px] dark:bg-input/30 rounded-md border"></div>
               <div className="h-[640px] w-[600px] flex flex-col justify-between">
                 <Textarea className="w-[600px] h-[560px]" placeholder="Type in your script or your script idea here. " />
                 <div className="flex flex-row justify-between h-[50px]">
